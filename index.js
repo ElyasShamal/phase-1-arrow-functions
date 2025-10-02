21c83a52e4098157e5df7cfc552ac119003e5def
ElyasShamal <Elyas.shamal2@gmail.com>
2025-10-01 23:14:11 -0400

    arrow function completed

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const divide = (a, b) => a / b;
+
+const square = number => number * number;
+
+const add = (a, b) => a + b
+
+console.log(divide(2000, 100))
+console.log(square(10))
+console.log(add(10, 10))
